e6a5ee748b50db771bd5baa97cf5f3d1c55859ef
Naitik Shah <shahnaitik2000@gmail.com>
2023-09-19 11:59:05 -0400

    Rectified code issues

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,6 +20,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+import python from "./tech/python.png"
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
@@ -54,6 +55,7 @@ export {
   threejs,
   meta,
   shopify,
+  python,
   starbucks,
   tesla,
   carrent,
